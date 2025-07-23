bc32d9c5713fc8299edccac8d51bf25227dd8b82
Syed Saad Ahmed <syedsaadahmed2094@yahoo.com>
2025-07-23 23:47:41 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
